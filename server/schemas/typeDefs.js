de6000b34e0132e87a67c66e1e3b855e7da9ad0c
vayne <miguel.osorio19@yahoo.com>
2022-04-10 16:10:57 -0500

    testing resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,12 @@
 // import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// creating the typeDefs
-const typeDefs = gql``;
+// creating the typeDefs -  Tagged templates are an advanced use of template literals
+const typeDefs = gql`
+  type Query {
+    helloWorld: String
+  }
+`;
 
 // export the typeDefs
 module.exports = typeDefs;
