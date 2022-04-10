0150c902d05e01695dc31d17b427552692e922db
vayne <miguel.osorio19@yahoo.com>
2022-04-10 18:33:10 -0500

    added Mutation in typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,28 +3,39 @@ const { gql } = require('apollo-server-express');
 
 // creating the typeDefs -  Tagged templates are an advanced use of template literals
 const typeDefs = gql`
+  # User definition
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
 
-    # User definition
-    type User {
-        _id: ID
-        username: String
-        email: String
-        bookCount: Int
-        savedBooks: [Book]
-    }
-
-    # Book definition
-    type Book {
-        bookId: ID!
-        authors: [String]
-        description: String!
-        image: String
-        link: String
-        title: String!        
-    }
+  # Book definition
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String!
+  }
 
   type Query {
     me: User
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
   }
 `;
 
