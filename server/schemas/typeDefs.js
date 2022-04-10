aa5796d4ac8112777c1715a743e9c72945b0fab8
vayne <miguel.osorio19@yahoo.com>
2022-04-10 17:10:13 -0500

    setting up the User model in typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,16 +4,18 @@ const { gql } = require('apollo-server-express');
 // creating the typeDefs -  Tagged templates are an advanced use of template literals
 const typeDefs = gql`
     type User {
-
+        _id: ID
+        username: String
+        email: String
     }
 
     type Book {
-        
+
     }
 
 
   type Query {
-    helloWorld: String
+    
   }
 `;
 
