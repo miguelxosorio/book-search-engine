91ea284064903fc2ec5cee9884b9fa47b28837f2
vayne <miguel.osorio19@yahoo.com>
2022-04-10 18:56:52 -0500

    setting up the me Query in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,15 +1,20 @@
-const { User, Book } = require('../models');
+const { User } = require('../models');
 
 const resolvers = {
+  // QUERY
   Query: {
-    
-    // get all users
-    users: async () => {
-        return User.find()
-        .select('-__v -password')
-        // .populate('books')
-    }
+    me: async (parent, args, context) => {
+      // check for the existence of context.user. If no context.user property exists, then we know that the user isn't authenticated and we can throw an AuthenticationError.
+      if (context.user) {
+        const userData = await User.findOne({})
+        .select('-__v -password');
+
+        return userData
+      }
+    },
   },
+
+  Mutation: {},
 };
 
-module.exports = resolvers; 
+module.exports = resolvers;
