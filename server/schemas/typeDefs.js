c07470afc928d56b94a040a4d6a7950fa9420cd8
vayne <miguel.osorio19@yahoo.com>
2022-04-10 17:13:21 -0500

    setting up the Book model in typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -10,7 +10,13 @@ const typeDefs = gql`
     }
 
     type Book {
-
+        _id: ID
+        authors: String
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String        
     }
 
 
