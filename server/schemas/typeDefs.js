9103838474a0198f3295d85e3191a1284b00f10b
vayne <miguel.osorio19@yahoo.com>
2022-04-10 16:07:56 -0500

    setting up the typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,8 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// creating the typeDefs
+const typeDefs = gql``;
+
+// export the typeDefs
+module.exports = typeDefs;
