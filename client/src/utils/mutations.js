83a4ee39cb3eaa4b67fdcf347123e9dd9f15e1ed
vayne <miguel.osorio19@yahoo.com>
2022-04-10 21:14:47 -0500

    updated the SAVE_BOOK mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -30,7 +30,23 @@ export const ADD_USER = gql`
 `;
 
 // SAVE_BOOK will execute the saveBook mutation.
-export const SAVE_BOOK = gql``;
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
 
 // REMOVE_BOOK will execute the removeBook mutation.
 export const REMOVE_BOOK = gql``;
