c6f1a96eeabc5596fb48f24a621275ee2b55a117
vayne <miguel.osorio19@yahoo.com>
2022-04-10 21:01:54 -0500

    setting upt he mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,8 +1,13 @@
+import { gql } from '@apollo/client';
+
 // LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
+export const LOGIN_USER = gql``;
 
 // ADD_USER will execute the addUser mutation.
+export const ADD_USER = gql``;
 
 // SAVE_BOOK will execute the saveBook mutation.
+export const SAVE_BOOK = gql``;
 
 // REMOVE_BOOK will execute the removeBook mutation.
-
+export const REMOVE_BOOK = gql``;
