b55307bfbf087b8cfcf450a7329cfcaefccefbbf
vayne <miguel.osorio19@yahoo.com>
2022-04-10 21:07:36 -0500

    updated the LOGIN_USER mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,20 @@
 import { gql } from '@apollo/client';
 
 // LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
-export const LOGIN_USER = gql``;
+
+// import the gql tagged template literal functionality to create a GraphQL mutation called login
+// This will accept two variables, $email and $password, whose values we'll set up to be passed in as arguments when we integrate this with the login form page
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
 
 // ADD_USER will execute the addUser mutation.
 export const ADD_USER = gql``;
