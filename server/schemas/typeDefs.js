2f1a0653acc63b263d7c075b145024d9ccf44bbc
vayne <miguel.osorio19@yahoo.com>
2022-04-11 00:00:46 -0500

    corrected wrong data type in type defs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -35,7 +35,7 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(bookData: BookInput!): User
+    saveBook(bookData: String!): User
     removeBook(bookId: ID!): User
   }
 `;
